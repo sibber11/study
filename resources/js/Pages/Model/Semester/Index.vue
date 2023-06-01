0f7a8dcda10e51c4119b08f09027a899d27bbbb6
sibber11 <sibber11@gmail.com>
2023-06-01 21:19:00 +0600

    semester index.vue added

---
+++ resources/js/Pages/Model/Semester/Index.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import ModelIndex from '@/Layouts/ModelIndex.vue';
+    defineProps(['semesters']);
+</script>
+
+<template>
+    <ModelIndex :models="semesters" title="Semesters"/>
+</template>
