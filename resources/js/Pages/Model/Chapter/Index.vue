4f5f42293978077799d2f89c929c829d0fc4cdfc
sibber11 <sibber11@gmail.com>
2023-06-02 18:27:30 +0600

    edit and delete added

--- resources/js/Pages/Model/Chapter/Index.vue
+++ resources/js/Pages/Model/Chapter/Index.vue
@@ -1,17 +1,26 @@
-<script setup>
+<script setup >
 import ModelIndex from '@/Layouts/ModelIndex.vue';
-import {Table} from '@protonemedia/inertiajs-tables-laravel-query-builder'
-defineProps(['chapters']);
+import {Table} from '@protonemedia/inertiajs-tables-laravel-query-builder';
+import EditButton from "@/Components/EditButton.vue";
+import DeleteButton from "@/Components/DeleteButton.vue";
+defineProps(['chapters', 'status'])
 </script>
 
 <template>
-    <ModelIndex title="Chapters">
+    <ModelIndex title="Chapters" :url="route('chapter.create')">
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
         <Table :resource="chapters">
             <template #cell(course)="{item:chapter}">
                 {{ chapter.course.name }}
             </template>
             <template #cell(semester)="{item:chapter}">
                 {{ chapter.course.semester.name }}
+            </template>
+            <template #cell(actions)="{ item: chapter}">
+                <EditButton :url="route('chapter.edit', chapter)"/>
+                <DeleteButton :url="route('chapter.destroy', chapter)"/>
             </template>
          </Table>
         
