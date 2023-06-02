7e9a68aff24676a9194448457accf86981487267
sibber11 <sibber11@gmail.com>
2023-06-02 18:26:33 +0600

    create button added

--- resources/js/Layouts/ModelIndex.vue
+++ resources/js/Layouts/ModelIndex.vue
@@ -1,25 +1,33 @@
 <script setup>
-    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-    import GuestLayout from '@/Layouts/GuestLayout.vue';
-    import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { Link } from '@inertiajs/vue3';
 
-    defineProps(['title']);
+defineProps(['title', 'url']);
 </script>
 
 <template>
-    <Head :title="title"/>
+    <Head :title="title" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="dark:text-gray-400">{{title}}</h2>
+            <div class="flex justify-between">
+                <h2 class="dark:text-gray-400">{{ title }}</h2>
+                <Link :href="url" v-if="url">
+                <PrimaryButton>
+                    Create
+                </PrimaryButton>
+                </Link>
+            </div>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                         <h3 class="text-2xl text-center">Laravel 9 Inertia JS Datatable</h3>
-                         <slot></slot>
+                        <h3 class="text-2xl text-center">Laravel 9 Inertia JS Datatable</h3>
+                        <slot></slot>
                     </div>
                 </div>
             </div>
