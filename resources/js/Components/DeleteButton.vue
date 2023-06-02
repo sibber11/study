ca2fa33bed6479f5011963cfca6533468965655f
sibber11 <sibber11@gmail.com>
2023-06-02 18:26:48 +0600

    button component added

---
+++ resources/js/Components/DeleteButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import {Link} from "@inertiajs/vue3";
+
+defineProps({
+    url: String
+})
+
+</script>
+
+<template>
+    <Link :href="url" as="button" method="delete" class="px-1 py-0.5 border rounded bg-red-600 text-white">
+        <!-- <i class="fa fa-trash-alt"></i> -->
+        <span>Delete</span>
+    </Link>
+</template>
---
+++ resources/js/Components/EditButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+const props = defineProps({
+    url: String
+})
+</script>
+<template>
+    <Link :href="url" class="px-1 py-0.5 border rounded">
+        <!-- <i class="fa fa-sm fa-pencil"></i> -->
+        <span>Edit</span>
+    </Link>
+</template>
