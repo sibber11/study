0b1c1fa9105451b36efda89550dc74c226987be1
sibber11 <sibber11@gmail.com>
2023-06-04 15:48:37 +0600

    cleaned file

--- resources/js/Pages/Model/Chapter/Create.vue
+++ resources/js/Pages/Model/Chapter/Create.vue
@@ -1,11 +1,5 @@
 <script setup>
 import CreateLayout from '@/Layouts/CreateLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import CancelButton from '@/Components/CancelButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, useForm } from '@inertiajs/vue3';
 import Fields from './Fields.vue';
 
 const props = defineProps(['courses', 'status']);
