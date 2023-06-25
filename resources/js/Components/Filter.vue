2553a791f25184630d6121521b39a45d5822b770
sibber11 <sibber11@gmail.com>
2023-06-25 19:14:29 +0600

    refactored for multi purpose

--- resources/js/Components/Filter.vue
+++ resources/js/Components/Filter.vue
@@ -1,24 +1,38 @@
 <script setup>
-import {useForm, usePage} from "@inertiajs/vue3";
+import {useForm} from "@inertiajs/vue3";
 
-defineProps(['courses'])
+const props = defineProps({
+        models: {
+            type: Object,
+            required: true
+        },
+        model_id: {
+            type: String,
+            required: true
+        },
+        route_name: {
+            type: String,
+            required: true
+        },
+    }
+)
 const form = useForm({
-    course_id: usePage().props.course_id ?? '',
+    model_id: props.model_id ?? '',
 })
 
 function submit() {
-    form.post(route('change_course'))
+    form.post(route(props.route_name))
 }
 </script>
 
 <template>
     <form @submit.prevent="submit">
-        <select id="topic" v-model="form.course_id"
+        <select id="model" v-model="form.model_id"
                 class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
-                name="course_id"
+                name="model_id"
                 @change="submit">
-            <option v-for="(course, id) in courses" :key="id" :value="id">{{ course }}</option>
-            <option value="">Choose a Course...</option>
+            <option v-for="(model, id) in models" :key="id" :value="id">{{ model }}</option>
+            <option value="">Choose an option...</option>
         </select>
     </form>
 </template>
