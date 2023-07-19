27fb9a78940df269cb07da755a012d212b3a5488
sibber11 <sibber11@gmail.com>
2023-07-19 21:30:06 +0600

    style fix

--- resources/js/Components/Filter.vue
+++ resources/js/Components/Filter.vue
@@ -30,11 +30,11 @@ function submit() {
 
 <template>
     <form @submit.prevent="submit">
-        <label for="model" v-if="label" class="block text-sm font-medium text-gray-700 dark:text-white">
-            {{ label }}
+        <label for="model" v-if="label" class="mb-2 block text-sm font-medium text-gray-700 dark:text-white">
+            Select {{ label }}
         </label>
         <select id="model" v-model="form.model_id"
-                class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                class="px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 w-full"
                 name="model_id"
                 @change="submit">
             <option v-for="(model, id) in models" :key="id" :value="id">{{ model }}</option>
